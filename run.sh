d3bbfcb906f7cbf8c60ef92b737684644a1f2a78
Tobias Boehm <tobias@jxx.ca>
2022-05-30 23:17:10 -0700

    Better run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 git pull
-docker build -t signal-msg-forwarder . && docker run --env-file=.env -d --restart=unless-stopped signal-msg-forwarder
+docker rm -f signalmsgforwarder
+docker build -t signal-msg-forwarder . && docker run --env-file=.env -d --restart=unless-stopped --name signalmsgforwarder signal-msg-forwarder
