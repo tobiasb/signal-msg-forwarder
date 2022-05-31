3458152fb28b71cf06aaff2727535dc0a07de424
Tobias Boehm <tobias@jxx.ca>
2022-05-30 22:49:09 -0700

    Make container run indefinitely

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git pull
-docker build -t signal-msg-forwarder . && docker run --env-file=.env -d signal-msg-forwarder
+docker build -t signal-msg-forwarder . && docker run --env-file=.env -d --restart=unless-stopped signal-msg-forwarder
