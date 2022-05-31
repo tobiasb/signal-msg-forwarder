7a8f8433158e30b46a574277905f22cf3e89f80a
Tobi <tobi@mbp-home.local>
2022-05-30 21:39:31 -0700

    Poll in loop

--- app.py
+++ app.py
@@ -3,27 +3,31 @@ from datetime import datetime
 import logging
 import requests
 import os
+from time import sleep
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
-response = requests.get(f"{os.getenv('SIGNAL_API_HOST')}/v1/receive/{os.getenv('SIGNAL_PHONE_NUMBER')}")
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.getenv('TABLE_NAME'))
 table.load()
 
-with table.batch_writer() as writer:
-    for msg in response.json():
-        envelope = msg['envelope']
+while True:
+    with table.batch_writer() as writer:
+        logger.info('Looking for messages')
+        response = requests.get(f"{os.getenv('SIGNAL_API_HOST')}/v1/receive/{os.getenv('SIGNAL_PHONE_NUMBER')}")
+        for msg in response.json():
+            envelope = msg['envelope']
 
-        logger.info(f"Writing message from {envelope['sourceNumber']}")
+            logger.info(f"Writing message from {envelope['sourceNumber']}")
 
-        writer.put_item(Item={
-            'timestamp': envelope['dataMessage']['timestamp'],
-            'fromNumber': envelope['sourceNumber'],
-            'fromName': envelope['sourceName'],
-            'message':  envelope['dataMessage']['message']
-        })
+            writer.put_item(Item={
+                'timestamp': envelope['dataMessage']['timestamp'],
+                'fromNumber': envelope['sourceNumber'],
+                'fromName': envelope['sourceName'],
+                'message':  envelope['dataMessage']['message']
+            })
 
-logger.info('Done')
+    logger.info('Done')
+    sleep(60)
