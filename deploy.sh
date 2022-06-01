d31a730a03f5dc9040d884d815519e3b982ddd8d
Tobias Boehm <tobias.boehm@getaround.com>
2022-06-01 13:20:43 -0700

    Add docker internal host

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 CONTAINER_NAME=signalmsgforwarder
 git pull
 docker rm -f "$CONTAINER_NAME"
-docker build -t signal-msg-forwarder . && docker run --env-file=.env -d --restart=unless-stopped --name "$CONTAINER_NAME" signal-msg-forwarder
+docker build -t signal-msg-forwarder . && docker run --add-host host.docker.internal:host-gateway --env-file=.env -d --restart=unless-stopped --name "$CONTAINER_NAME" signal-msg-forwarder
