368e0f35eeae899a2370639f0758af3d010ccaa4
Tobias Boehm <tobias.boehm@getaround.com>
2022-06-07 09:03:32 -0700

    Add db dump script

---
+++ dump_database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+echo "Messages:"
+echo "-----------------------------------------"
+sqlite3 "$1" "select * from messages;"
+echo "-----------------------------------------"
+echo 
+echo "Attachments:"
+echo "-----------------------------------------"
+sqlite3 "$1" "select * from attachments;"
+echo "-----------------------------------------"
